e8dc2faac4483d6004ce2fec7004dd2108bf7a68
mjt <thapa.manish16@gmail.com>
2019-05-28 10:51:48 +0545

    templeting informations

--- server/app/api/api.controller.js
+++ server/app/api/api.controller.js
@@ -5,16 +5,53 @@ var qr = require("../../services/qr_code");
 
 var JwtHandler = require("../../services/jwt_handlers");
 
+var emailTemplate = require("../../services/get_templete");
+
 router.get("/:email", function(req, res) {
   var email = req.params.email;
   var token = JwtHandler.createJwt({
     email: email,
-    name: "helloworld"
+    name: "test"
   });
+  //   res.type("png");
+  //   arr = token.split(".");
+  //   token = arr[1] + "." + arr[2];
+
+  var img = qr.createQr(token).toString("base64");
+  //   var html = '<img src="data:image/png;base64,' + img + '" />';
+  var data = {
+    name: "test",
+    token: token,
+    imgBase64: img
+  };
+
+  var html = emailTemplate(data);
+  return res.send(html);
+});
+
+router.post("/create", function(req, res) {
+  var body = req.body;
+  console.log(body);
+  if (typeof req.body.name == "undefined" || req.body.name == "") {
+    return res.send("no name provided");
+  }
+
+  if (typeof req.body.email == "undefined" || req.body.email == "") {
+    return res.send("no email provided");
+  }
+
+  var token = JwtHandler.createJwt(body);
   //   res.type("png");
 
   var img = qr.createQr(token).toString("base64");
-  var html = '<img src="data:image/png;base64,' + img + '" />';
+  //   var html = '<img src="data:image/png;base64,' + img + '" />';
+  var data = {
+    name: req.body.name,
+    token: token,
+    imgBase64: img
+  };
+
+  var html = emailTemplate(data);
   return res.send(html);
 });
 
