9a57215b00fd2990076678f92ca8fae9a8ca6bd0
Manish Jung thapa <thapa.manish16@gmail.com>
2019-10-01 23:14:39 +0545

    removing comments

--- server/services/mail.js
+++ server/services/mail.js
@@ -13,15 +13,6 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-/* 
-const mailOptions = {
-  from: "thapa.manish16@gmail.com", // sender address
-  to: "thapa.manish16@gmail.com", // list of receivers
-  subject: "Subject of your email", // Subject line
-  html: "<p>Your html here</p>" // plain text body
-};
-*/
-
 var sendMail = function(mailOptions, callback) {
   transporter.sendMail(mailOptions, callback);
 };
